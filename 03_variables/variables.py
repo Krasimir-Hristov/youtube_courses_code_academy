25069531cf12840fd25ac2d6a831d990b966f7e6
krasi <krasimir.xristov@gmail.com>
2026-06-06 17:41:33 +0200

    03_variables

---
+++ 03_variables/variables.py
@@ -0,0 +1,43 @@
+# print(5)
+
+# x = 5
+# print(x)
+
+# name = "Ana"
+# age = 25
+# height = 1.75
+# is_student = True
+# var_list = []
+# var_dict = {}
+
+# print("name:", type(name))
+# print("age:", type(age))
+# print("height:", type(height))
+# print("is_student:", type(is_student))
+# print("var_list:", type(var_list))
+# print("var_dict:", type(var_dict))
+
+# name = "Ana"
+# print(name)
+# name = "Gosho"
+# print(name)
+
+# name, last_name, age = ["Ana", "Velkova", 5]
+
+# print(name, last_name, age)
+
+# a, b = 1, 2
+# print("before:", a, b)
+# b, a = a, b
+# print("after:", a, b)
+
+a = [
+    1,
+    2,
+    3,
+]
+
+b = a
+
+b.append(4)
+print(a)
