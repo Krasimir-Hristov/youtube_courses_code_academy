130563367e8d9dab698959b8ab411cab25f502f8
krasi <krasimir.xristov@gmail.com>
2026-06-08 22:37:53 +0200

    add naming conventions script with examples and best practices

---
+++ 04_naming/naming_conventions.py
@@ -0,0 +1,60 @@
+first_name = "Gosho"  # This is Python
+is_admin = True
+
+# def fetch_user_by_id(user_id):
+firstName = "Gosho"  # This not Python
+# print(first_name)
+# print(firstName)
+
+# Constants
+MAX_RETRIES = 3
+API_BASE_URL = "https://api.openai.com/v1"
+
+# print(MAX_RETRIES)
+# print("after")
+MAX_RETRIES = 5
+# print(MAX_RETRIES)
+
+
+# PascalCase
+class UserAccount:
+    pass
+
+
+class HTTPClient:
+    pass
+
+
+class Cache:
+    def __init__(self):
+        self.size = 100  # Public
+        self._item = {}  # Privet
+
+
+# def _cleanup(self): # internal method
+
+my_cache = Cache()
+my_cache.size = 200
+# print(my_cache.size)
+
+# False, True, break, and, or, as, async, await, finally, yield, with, while, for, class,  def, global, continue
+# False = True
+# print(False)
+# print = "my name"
+# print(print)
+# my_list = list(range(5))
+# print(list(range(5)))
+
+
+# Bad naming!!!!
+def calc(x, y):
+    z = x * y * 0.2
+    return z
+
+
+# Good Naming
+TAX_RATE = 0.2
+
+
+def calculate_tax(price: float, quantity: int) -> float:
+    return price * quantity * TAX_RATE
